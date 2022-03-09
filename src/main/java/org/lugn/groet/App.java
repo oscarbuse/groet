d44cd1d2c2bb02cfa6a1100df00461edbfe3fd56
vagrant <vagrant@vm2>
2022-03-09 19:51:35 +0000

    initial commit

---
+++ src/main/java/org/lugn/groet/App.java
@@ -0,0 +1,13 @@
+package org.lugn.groet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
